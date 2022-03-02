13a7a08f70b1f94128412088235509a2b2108e36
SamusMoran <kimberlym4488@comcast.net>
2022-03-02 14:13:23 -0800

    Fixed linting error

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Space, Idea, Interest, Comment } = require('../models');
+const { Space } = require('../models');
 const withAuth = require('../utils/auth');
 
 //Home/Dashboard
