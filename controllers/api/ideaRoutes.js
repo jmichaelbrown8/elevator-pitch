85ebe0d58ca40196e8fffe535ce43f5f8c5d9612
Jonathan Chen <jontkc98@uw.edu>
2022-03-02 01:06:45 -0800

    Added get and post method for interestRoutes.js

--- controllers/api/ideaRoutes.js
+++ controllers/api/ideaRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Idea } = require('../../models');
 
+router.post('/');
 
 module.exports = router;
--- controllers/api/interestRoutes.js
+++ controllers/api/interestRoutes.js
@@ -1,5 +1,47 @@
 const router = require('express').Router();
 const { Interest } = require('../../models');
 
+// Input:   idea_id
+// Output:  JSON Array containing every user that is interested in the idea
+router.get('/', async (req, res) => {
+  try {
+    const interestData = await Interest.findAll({
+      attributes: ['user_id'],
+      where: {
+        idea_id: req.body.idea_id,
+      },
+    });
+
+    res.status(200).json({ interestData });
+  } catch (err) {
+    let message = 'Something went wrong.';
+
+    res.status(400).json({
+      message,
+      err,
+    });
+  }
+});
+
+// Input:   idea_id, user_id
+// Creates a new interest relation between user and idea
+router.post('/', async (req, res) => {
+  try {
+    await Interest.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json({
+      message: `Interest created`,
+    });
+  } catch (err) {
+    let message = 'Something went wrong.';
+
+    res.status(400).json({
+      message,
+      err,
+    });
+  }
+});
 
 module.exports = router;
