3c3b93c11f74898f31ee76052b7935030975a1fa
Anthony Brown <ac524.brown@gmail.com>
2022-03-05 22:38:05 -0800

    Request access client js - Create `access.js` for send the POST to `/api/space/:space_id/join` - Added script link to view.

---
+++ public/js/access.js
@@ -0,0 +1,27 @@
+(() => {
+  const joinSpace = async ({ target }) => {
+    const space_id = target.dataset.id;
+
+    const response = await fetch(`/api/space/${space_id}/join`, {
+      method: 'POST',
+      body: JSON.stringify({ space_id }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      localStorage.setItem('toast', 'Your request for access has been sent.');
+      document.location.replace(`/space/${space_id}`);
+    } else {
+      localStorage.setItem('toast', 'Failed to request access.');
+      toastIt(true);
+    }
+  };
+
+  const joinButton = document.getElementById('access-space');
+
+  if (joinButton) {
+    joinButton.addEventListener('click', joinSpace);
+  }
+})();
