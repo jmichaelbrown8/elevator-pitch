e2b0c87495a7a4f94fb78f6b45be07912f7e1a32
J. Michael Brown <jmichaelbrown8@gmail.com>
2022-03-26 21:00:16 -0700

    Added endpoints for abandoning and claiming an idea

--- controllers/api/ideaRoutes.js
+++ controllers/api/ideaRoutes.js
@@ -51,9 +51,9 @@ router.delete(
       await Idea.destroy({
         where: {
           id: idea_id,
-          user_id
+          user_id,
         },
-        individualHooks: true
+        individualHooks: true,
       });
       res.status(200).json({ message: 'Idea deleted' });
     } catch (err) {
@@ -61,6 +61,72 @@ router.delete(
       console.log(err);
       res.status(400).json({
         message,
+        err,
+      });
+    }
+  }
+);
+
+// Abandon an idea
+router.put(
+  `${basePath}/:idea_id/abandon`,
+  withApprovedMembership, // must be space member
+  withIdeaOwnership, // must be idea owner
+  withAuthJson, // must be logged in (of course)
+  async (req, res) => {
+    const { user_id } = req.session;
+    const { idea_id } = req.params;
+    try {
+      await Idea.update(
+        {
+          user_id: null,
+        },
+        {
+          where: {
+            id: idea_id,
+          },
+        }
+      );
+      res.status(200).json({ message: 'Idea abandoned' });
+    } catch (err) {
+      res.status(400).json({
+        message: `User ${user_id} failed to abandon idea ${idea_id}`,
+        err,
+      });
+    }
+  }
+);
+
+// Claim an idea
+router.put(
+  `${basePath}/:idea_id/claim`,
+  withApprovedMembership, // must be a space member
+  withNoIdeaApprovals, // must not be approved for another idea
+  withAuthJson, // must be logged in
+  async (req, res) => {
+    const { user_id } = req.session;
+    const { idea_id } = req.params;
+    try {
+      // ensure idea has no current owner
+      const ideaData = await Idea.findByPk(idea_id);
+      const idea = ideaData.toJSON();
+      if (idea.user_id) {
+        throw `This idea ${idea_id} is already claimed by a user ${user_id}`;
+      }
+      await Idea.update(
+        {
+          user_id,
+        },
+        {
+          where: {
+            id: idea_id,
+          },
+        }
+      );
+      res.status(200).json({ message: 'Idea claimed' });
+    } catch (err) {
+      res.status(400).json({
+        message: `User ${user_id} failed to claim idea ${idea_id}`,
         err,
       });
     }
